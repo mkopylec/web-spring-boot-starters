0c31ff4bc0ab4e11b4bc89db08a11c948adf8691
Mariusz Kopylec <mariusz.kopylec@o2.pl>
2015-10-24 14:09:15 +0200

    Exception rename

--- web-backend-spring-boot-starter/src/main/java/com/github/mkopylec/webbackend/exceptions/NoResultException.java
+++ web-backend-spring-boot-starter/src/main/java/com/github/mkopylec/webbackend/exceptions/NoResultException.java
@@ -2,9 +2,9 @@ package com.github.mkopylec.webbackend.exceptions;
 
 import static javax.ws.rs.core.Response.Status.NOT_FOUND;
 
-public class NotFoundException extends ApplicationException {
+public class NoResultException extends ApplicationException {
 
-    public NotFoundException(String errorCode, String message) {
+    public NoResultException(String errorCode, String message) {
         super(errorCode, message, NOT_FOUND);
     }
 }
