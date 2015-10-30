62b20fc33a317b8807599d95ef3e58e7156d2268
Mariusz Kopylec <mariusz.kopylec@o2.pl>
2015-10-30 20:40:01 +0100

    Security disabling config moved to autoconfiguration

--- web-backend-spring-boot-starter/src/main/java/com/github/mkopylec/webbackend/security/SecurityDisabledConfiguration.java
+++ web-backend-spring-boot-starter/src/main/java/com/github/mkopylec/webbackend/security/SecurityDisabledConfiguration.java
@@ -2,16 +2,12 @@ package com.github.mkopylec.webbackend.security;
 
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
-import static org.springframework.boot.autoconfigure.security.SecurityProperties.ACCESS_OVERRIDE_ORDER;
-
 @Configuration
 @EnableWebSecurity
-@Order(ACCESS_OVERRIDE_ORDER)
 @ConditionalOnMissingBean(SecurityConfiguration.class)
 public class SecurityDisabledConfiguration extends WebSecurityConfigurerAdapter {
 
