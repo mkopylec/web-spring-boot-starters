f660abe27f8f48f763a08ada539b16109b0cb6c5
Mariusz Kopylec <mariusz.kopylec@o2.pl>
2015-10-30 18:00:15 +0100

    @Configuration added on method sec config

--- web-backend-spring-boot-starter/src/main/java/com/github/mkopylec/webbackend/security/MethodSecurityConfiguration.java
+++ web-backend-spring-boot-starter/src/main/java/com/github/mkopylec/webbackend/security/MethodSecurityConfiguration.java
@@ -1,10 +1,12 @@
 package com.github.mkopylec.webbackend.security;
 
 import com.github.mkopylec.webbackend.security.authorization.TokenAuthorizationProvider;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.method.configuration.GlobalMethodSecurityConfiguration;
 
+@Configuration
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class MethodSecurityConfiguration extends GlobalMethodSecurityConfiguration {
 
